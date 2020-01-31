dc21d55e846138f82b7c378a8def357eb0f493e9
beeinger <sulecki.maciek@gmail.com>
2020-01-31 15:13:21 +0100

    hotfix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 const withPlugins = require("next-compose-plugins");
-const optimizedImages = require("next-optimized-images");
 const progressBar = require("next-progressbar");
 const offline = require("next-offline");
 
 module.exports = withPlugins([
-  [optimizedImages, {}],
   [progressBar, {}],
   [
     offline,
