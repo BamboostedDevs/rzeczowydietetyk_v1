27cc1cf2947be72186f0675303a0f09a2b4c2d19
beeinger <sulecki.maciek@gmail.com>
2020-01-31 15:41:05 +0100

    hotfix

--- pages/_app.js
+++ pages/_app.js
@@ -7,12 +7,14 @@ export default function MyApp({ Component, pageProps }) {
     <Layout>
       <Component {...pageProps} />
       <div style={{ display: "none" }}>
-        <img src="/images/owocnewarzywa.svg" />
+        <img src="/images/avocadoT.svg" />
+        <img src="/images/cover.jpg" />
         <img src="/images/office_web.jpg" />
-        <img src="/images/tanita_web.jpg" />
-        <img src="/images/owocnewarzywa_mobile.svg" />
-        <img src="/images/tanita_mobile.jpg" />
         <img src="/images/office_mobile.jpg" />
+        <img src="/images/owocnewarzywa.svg" />
+        <img src="/images/owocnewarzywa_mobile.svg" />
+        <img src="/images/tanita_web.jpg" />
+        <img src="/images/tanita_mobile.jpg" />
       </div>
     </Layout>
   );
